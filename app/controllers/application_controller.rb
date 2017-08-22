a17819bf83b3f2f19dd073ba7f1758b37b842571
jinstrider2000 <efrainperezjr@live.com>
2017-08-22 10:52:17 -0400

    updated cart_initialization to reflect changes to User class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,38 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :current_cart
-
-
-  def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || root_path
-  end
+  helper_method :current_cart
 
   private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
   def initialize_cart
-    @cart = current_user.current_cart
+    @cart = current_cart
     if @cart.nil?
-      @cart = Cart.create
-      @cart.user = current_user
-      current_user.current_cart = @cart
-      current_user.save
-      @cart.save
+      current_cart = Cart.create
+      @cart = current_cart
     end
   end
 
   def current_cart=(cart)
-    User.cart_id = cart.id if cart
-    @current_cart = cart
-    User.save
+    current_user.current_cart = cart
   end
 
   def current_cart
-    @current_cart = Cart.find_by(id: User.cart_id)
+    current_user.current_cart
   end
 
 end
