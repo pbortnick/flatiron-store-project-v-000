be71f3034b3cfbd754d4bc28a5137cc845e496eb
jinstrider2000 <efrainperezjr@live.com>
2017-08-23 08:27:31 -0400

    eliminated unnecessary files and code redundancies

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'store#index', as: 'store'
 
-  resources :items, only: [:show, :index]
-  resources :categories, only: [:show, :index]
-  resources :carts
+  resources :items, only: [:show]
+  resources :categories, only: [:show]
+  resources :carts, only: [:show]
   post '/line_items/:item_id' => 'line_items#create', as: "line_items"
   resources :orders, only: [:show]
 
